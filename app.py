36c5dced447780830a845a8b7de05589d00fc61a
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-09 13:55:51 +0900

    fix

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.write("# 6300원의 행복")
 col1, col2 = st.columns(2)
 
 col1.write("## The 좋은밥상")
-show_menus(col, "_xfWxfCxj")
+show_menus(col1, "_xfWxfCxj")
 
 col2.write("## 우림구내식당")
-show_menus(co2, "_ixcNxexj")
+show_menus(col2, "_ixcNxexj")
