6ddc306d1cf7c1fc163ae41887047fcce652089a
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-09 13:40:20 +0900

    api

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
-
+import requests
 st.write("# 6300원의 행복")
 
 col1, col2 = st.columns(2)
 
 col1.write("## The 좋은밥상")
+url = "https://pf-wapi.kakao.com/web/profiles/_xfWxfCxj/posts"
+req = requests.get(url)
+json = req.json()
+st.write(json)
 
 col2.write("## 우림구내식당")
