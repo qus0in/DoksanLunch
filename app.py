4ac6b181ab0e144a9062f6ab1abd79ca1cf6fe92
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-09 15:11:03 +0900

    12

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.write("## The 좋은밥상")
 df = pd.DataFrame(
     [[37.4682657, 126.886182]],
     columns=['lat', 'lon'])
-st.map(df, zoom=15)
+st.map(df, zoom=12)
 show_menus("_xfWxfCxj")
 st.write("## 우림구내식당")
 show_menus("_ixcNxexj")
