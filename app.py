4375a251e53f5ed8c364d817530ee886274cff73
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-09 13:55:34 +0900

    refactoring

--- app.py
+++ app.py
@@ -14,24 +14,21 @@ def get_media(id, post_id):
     media = json["media"]
     return media
 
+def show_menus(col, store_id):
+    posts = get_posts(store_id)
+    post_ids = [p['id'] for p in posts['items']]
+    for post_id in post_ids:
+        media = get_media(store_id, post_id)
+        for m in media:
+            col.image(m['xlarge_url'])
+    col.write(post_ids)
+
 st.write("# 6300원의 행복")
 
 col1, col2 = st.columns(2)
 
 col1.write("## The 좋은밥상")
-store1 = "_xfWxfCxj"
-posts = get_posts(store1)
-post_id = posts['items'][0]['id']
-media = get_media(store1, post_id)
-for m in media:
-    col1.image(m['xlarge_url'])
+show_menus(col, "_xfWxfCxj")
 
 col2.write("## 우림구내식당")
-store2 = "_ixcNxexj"
-posts = get_posts(store2)
-post_ids = [p['id'] for p in posts['items']]
-for post_id in post_ids:
-    media = get_media(store2, post_id)
-    for m in media:
-        col2.image(m['xlarge_url'])
-col2.write(post_ids)
+show_menus(co2, "_ixcNxexj")
