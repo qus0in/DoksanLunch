18e02d71647ae14725ad792da18ac39a1c117e81
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-03-09 13:51:55 +0900

    ids

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 import streamlit as st
 import requests
+
+def get_posts(id):
+    url = f"https://pf-wapi.kakao.com/web/profiles/{id}/posts"
+    req = requests.get(url)
+    json = req.json()
+    return json
+
+def get_media(id, post_id):
+    url = f"https://pf-wapi.kakao.com/web/profiles/{id}/posts/{post_id}"
+    req = requests.get(url)
+    json = req.json()
+    media = json["media"]
+    return media
+
 st.write("# 6300원의 행복")
 
 col1, col2 = st.columns(2)
 
 col1.write("## The 좋은밥상")
-url = "https://pf-wapi.kakao.com/web/profiles/_xfWxfCxj/posts"
-req = requests.get(url)
-json = req.json()
-# col1.write(json['items'][0]['id'])
-id = json['items'][0]['id']
-req = requests.get(f"{url}/{id}")
-json = req.json()
-media = json["media"]
+store1 = "_xfWxfCxj"
+posts = get_posts(store1)
+post_id = posts['items'][0]['id']
+media = get_media(store1, post_id)
 for m in media:
     col1.image(m['xlarge_url'])
 
 col2.write("## 우림구내식당")
+store2 = "_ixcNxexj"
+posts = get_posts(store2)
+post_ids = [p for p in posts['items']]
+col2.write(post_ids)
